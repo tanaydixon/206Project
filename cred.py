4fbe6fc582b7aae9ab140d4d6064456039c82481
rahatn202 <112509219+rahatn202@users.noreply.github.com>
2022-12-08 14:49:03 -0500

    added cred,py file

---
+++ cred.py
@@ -0,0 +1,6 @@
+client_id = 'fb6788164b47401c8cf81b4d3271fb8a'
+client_secret = '9d7350852f324c24a6dbcd66dd14b56b'
+redirect_uri='https://mysite.com/rahat/tanay/'
+
+
+# userid =     "316qlwtjr4yhyleissfe6vis6iqi"
