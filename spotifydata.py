5d2658536a54885076a512062b5437e3c992d56f
rahatn202 <112509219+rahatn202@users.noreply.github.com>
2022-12-09 15:51:33 -0500

    added

--- spotifydata.py
+++ spotifydata.py
@@ -24,27 +24,30 @@ def user_info(spotify):
 def create_playlist(spotify):
     #user_id = spotify.user_info().get('id', "None")
     top_50_usa_data = spotify.playlist_tracks('37i9dQZF1DXcBWIGoYBM5M')
+   
     top_50_uk_data = spotify.playlist_tracks('153yGNYdzvyCZxzDnIzNUx')
     song_tuple_list = []
 
     rank_counter = 1 
     for song in top_50_usa_data['items']:
+        song_id = song['track']['id']
         song_title = song['track']['name']
         song_artist = song['track']['artists'][0]['name']
         song_pop= song['track']['popularity']
         song_date = song['track']['album']['release_date']
         song_rank = rank_counter
-        song_tuple_list.append((song_title, song_artist, song_rank, song_date, song_pop, "usa"))
+        song_tuple_list.append((song_id, song_title, song_artist, song_rank, song_date, song_pop, "usa"))
         rank_counter += 1 
 
     rank_counter = 1 
     for song in top_50_uk_data['items']:
+        song_id = song['track']['id']
         song_title = song['track']['name']
         song_artist = song['track']['artists'][0]['name']
         song_pop= song['track']['popularity']
         song_date = song['track']['album']['release_date']
         song_rank = rank_counter
-        song_tuple_list.append((song_title, song_artist, song_rank, song_date, song_pop, "uk"))
+        song_tuple_list.append((song_id, song_title, song_artist, song_rank, song_date, song_pop, "uk"))
         rank_counter += 1 
     return song_tuple_list
 
@@ -63,17 +66,18 @@ def setUpDatabase(db_name):
     return cur, conn
 
 def createDatabase(cur, conn, spotify):
-    cur.execute("CREATE TABLE IF NOT EXISTS Spotify (song_title TEXT, song_artist TEXT, song_rank INTEGER, song_date TEXT, song_pop INTEGER, country_code TEXT)") 
+    cur.execute("CREATE TABLE IF NOT EXISTS Spotify (song_id TEXT, song_title TEXT, song_artist TEXT, song_rank INTEGER, song_date TEXT, song_pop INTEGER, country_code TEXT)") 
     cur.execute("SELECT COUNT(*) FROM Spotify")
     add_25 = cur.fetchone()[0]
     for item in create_playlist(spotify)[add_25:add_25+25]:
-        cur.execute("INSERT INTO Spotify (song_title, song_artist, song_rank, song_date, song_pop, country_code) VALUES (?, ?, ?, ?, ?, ?)", (item[0], item[1], item[2], item[3], item[4], item[5]))
+        cur.execute("INSERT INTO Spotify (song_id, song_title, song_artist, song_rank, song_date, song_pop, country_code) VALUES (?, ?, ?, ?, ?, ?, ?)", (item[0], item[1], item[2], item[3], item[4], item[5], item[5]))
         add_25 += 1
     conn.commit()
 
 
 def main():
     spotify = getSpotifyObject("7tj4dlofb2yvuijru40p3grnp", 'playlist-modify-public')
+    
     cur, conn = setUpDatabase('Billboard.db')
     createDatabase(cur, conn, spotify)
     conn.close()
